be0199f06daacd42bb508dca5c58beea266428fe
kevin podcast <sivadnivek@gmail.com>
2026-01-19 02:33:36 -0500

    Update simple-hostname-bl.sh

--- simple-hostname-bl.sh
+++ simple-hostname-bl.sh
@@ -10,16 +10,16 @@ BLIP="[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
 for i in ~/blip/hostnamestobl.txt; do dig +short "$i" | grep -oP "$BLIP" >> ~/blip/masterlist.ip ; done ;
 
 # dedupe the master list against what's already in ipset list
-ipset list blocklist > ~/blip/ipsetblocklist.ip; #something else leading to create deduped.txt
+ipset list blocklist > ~/blip/ipsetblocklist.ip; diff masterlist.ip ipsetblocklist.ip | grep \> > deduped.txt;
+sed -i 's/> //g' deduped.txt;
 
 # add what's left to the blocklist
-# this aint quite right ...... for yup in ~/blip/deduped.txt; do ipset add "$yup" blocklist 
 while read yip; do ipset add blocklist $yip; done < ~/blip/deduped.txt 
 
-# make sure you didn't block your own ip
-curl zx2c4.com/ip | grep -oP $BLIP > ~/blip/myip.now;
+# make sure you didn't block your own ip (or list of ips?)
+curl zx2c4.com/ip | grep -oP $BLIP > ~/blip/myip?s.now;
 
-if [ (ipset list | grep ~/blip/myip.now) -ge 1 ] then
+if [ (ipset list | grep ~/blip/myip.now) -ge 1 ] then # still not sure if this part works
 
 ipset del blocklist ~/blip/myip.now
 
@@ -30,3 +30,5 @@ done ;
 # maybe include an option to explicitly unblock some ips after checking for them?
 # please be aware that there may be a compatibility issue with the other related script i have on here
 # i think in that one i named the blocklist blacklist
+# fuck i balmer peaked and burned the bacon i was cooking to help with my hangover
+# house full of smoke
